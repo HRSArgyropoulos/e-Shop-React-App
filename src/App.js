02008d2f309663cde20bb79b44869d272fae1c2d
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-07-24 00:58:19 +0300

    styled components + switch for main content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import styled from 'styled-components';
+import MainContent from './components/MainContent';
+
+const AppWrapper = styled.div`
+  display: flex;
+  width: var(--dashboardWidth);
+  height: var(--dashboardHeight);
+  background-color: rgb(240, 240, 240);
+  margin: calc((100vh - var(--dashboardHeight)) / 2)
+    auto;
+  border-radius: var(--dashboardRadius);
+`;
+
+const paths = ['About', 'Products', 'Contact Us'];
 
 function App() {
   return (
-    <div className="App">
+    <AppWrapper className="App">
       <Router>
-        <header className="App-header">
-          <Navbar
-            paths={['About', 'Products', 'Contact Us']}
-            className="navbar"
-          />
-        </header>
+        {/* Sidebar */}
+        <Navbar paths={paths} className="navbar" />
+        {/* Main content */}
+        <MainContent />
       </Router>
-    </div>
+    </AppWrapper>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavbarWrapper = styled.div`
+  background-color: rgb(248, 230, 192);
+  color: black;
+  padding: 1rem 2rem;
+  width: 300px;
+  height: 100%;
+  border-radius: var(--dashboardRadius) 0 0
+    var(--dashboardRadius);
+  position: relative;
+`;
 
 const Navbar = (props) => {
   const { paths, className: navClass } = props;
   return (
-    <div className={navClass}>
+    <NavbarWrapper className={navClass}>
       <nav>
         <ul>
           <li key="homepage">
@@ -35,7 +47,7 @@ const Navbar = (props) => {
           })}
         </ul>
       </nav>
-    </div>
+    </NavbarWrapper>
   );
 };
 
